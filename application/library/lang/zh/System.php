28050b0fdd15fa82ed7851e3f063ff44fd46e4f7
Frank Li <franklee65535@gmail.com>
2018-01-12 14:45:09 +1300

    Rename the system name

--- application/library/lang/zh/System.php
+++ application/library/lang/zh/System.php
@@ -2,7 +2,7 @@
 
 class Lang_Zh_System {
 
-    const SYSTEMNAME = 'EASYISERVICE员工管理后台';
+    const SYSTEMNAME = '员工管理后台';
     const CHANGEPASSWORD = '修改密码';
     const LOGOUT = '登出';
     const WELCOMEMSG = '欢迎';
